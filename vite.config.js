c0636371c75cdd35f40a4026a51a1594eefd95d3
jai-prakaash <149857548+jai-prakaash@users.noreply.github.com>
2024-12-12 20:54:31 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/git 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/school/', // Set this to your project name 'school'
 })
